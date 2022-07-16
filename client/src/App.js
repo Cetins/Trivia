56c8061cb0cf302249d69da0fb0106e9247832f4
Cetins <69481095+Cetins@users.noreply.github.com>
2022-07-16 09:11:12 +0100

    develop branch added, under construction

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This project is under construction</h1>
+      <p>...and the owner of this project is a backend dev</p>
+      <p>that's why there isn't any fancy illustrations.</p>
     </div>
   );
 }
